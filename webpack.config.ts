5d2f0e1f84d232a8792d97931aeb054249e74dbb
Luke Sheard <me@lukesheard.com>
2017-12-30 14:21:37 +0000

    Add travis script

--- webpack.config.ts
+++ webpack.config.ts
@@ -6,7 +6,7 @@ export default function(env: string) {
   const PROD = env === "production";
 
   const plugins: webpack.Plugin[] = [
-    new ExtractTextPlugin(`packages/[name]/dist/[name]${PROD ? ".min" : ""}.css`),
+    new ExtractTextPlugin(`[name]${PROD ? ".min" : ""}.css`),
   ];
 
   if (PROD) {
@@ -18,12 +18,14 @@ export default function(env: string) {
   const config: webpack.Configuration = {
     context: __dirname,
     devtool: PROD ? false : "source-map",
-    entry: [
-      "@phosphor/widgets",
-      "@phosphor/widgets/style",
-    ],
+    entry: {
+      phosphor: [
+        "@phosphor/widgets",
+        "@phosphor/widgets/style",
+      ]
+    },
     output: {
-      filename: `phosphor${PROD ? ".min" : ""}.js`,
+      filename: `[name]${PROD ? ".min" : ""}.js`,
       path: join(__dirname, "dist"),
       library: "phosphor",
       libraryTarget: "umd"
